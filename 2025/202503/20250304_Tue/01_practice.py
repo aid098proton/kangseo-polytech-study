e927076f70dc0472b542115b1b9928f1a0847e25
aid098proton <aid098proton@gmail.com>
2025-04-24 19:43:58 +0900

    기본 라이브러리
    기본 라이브러리

---
+++ 2025/202503/20250304_Tue/01_practice.py
@@ -0,0 +1,9 @@
+# prompt: 데이터분석 기본 라이브러리 선언해줘
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 추가적으로 필요한 라이브러리가 있다면 이곳에 추가합니다.
+# 예시: from scipy import stats
